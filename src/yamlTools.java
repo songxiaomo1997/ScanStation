b1f6a46456b4fb6bf38bbfd3f93f49530f188d18
songxiaomo1997 <86089788+songxiaomo1997@users.noreply.github.com>
2021-06-18 13:18:12 +0800

    Add files via upload

---
+++ src/yamlTools.java
@@ -0,0 +1,30 @@
+package com.scanStation.tools;
+
+import com.scanStation.bean.vulBean;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+public class yamlTools {
+    private String filepath;
+
+    public yamlTools(String filepath) {
+        this.filepath = filepath;
+    }
+
+
+    public vulBean load() {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(filepath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        Yaml yaml = new Yaml();
+        vulBean vulBean = yaml.loadAs(inputStream, vulBean.class);
+        return vulBean;
+    }
+
+}
